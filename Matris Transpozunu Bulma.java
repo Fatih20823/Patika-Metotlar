53360c7510abe861a3514c3d1089bf6528952229
Fatih20823 <101557027+Fatih20823@users.noreply.github.com>
2023-01-18 13:59:06 +0300

    Update Matris Transpozunu Bulma.java

--- Matris Transpozunu Bulma.java
+++ Matris Transpozunu Bulma.java
@@ -7,19 +7,20 @@ public class Application {
 		
 		int[][] a = {{2,3,4},
 					{5,6,4}};
-		int[][] aT =new int[3][2];
+		int[][] aT =new int[a[0].length][a.length];
 			
 		for (int row = 0; row < a.length; row++) {
-			for (int col = 0; col < a.length+1; col++) {
+			for (int col = 0; col < a[row].length; col++) {
 				         aT[col][row]=a[row][col];
-			}
-			
+			}			
 		}
 
-		for (int[] row : aT) {
-			for (int col : row) {
-				System.out.println(col);
-			}
-		}
+		for (int i =0 ; i < aT.length ; i++){
+            for (int j = 0; j <  aT[i].length; j++) {
+                System.out.print(aT[i][j] + "     ");
+            }
+            System.out.println();
+        }
 	}	
 }
+
